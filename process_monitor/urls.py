f2f71fa468c936c34b4eb6b8c2404656b41ed6c4
Szymon Zak <szymer@gmail.com>
2025-03-21 22:51:34 +0100

    ..

--- process_monitor/urls.py
+++ process_monitor/urls.py
@@ -18,10 +18,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from process_monitor_app.views import (ExportSnapshotView, HomeView, LoginView,
-                                       ProcessListView, SnapshotDetailedView,
-                                       SnapshotListView, SnapshotView,
-                                       StoppedProcessesView, StopProcessView)
+from process_monitor_app.views import (
+    ExportSnapshotView,
+    HomeView,
+    LoginView,
+    ProcessListView,
+    SnapshotDetailedView,
+    SnapshotListView,
+    SnapshotView,
+    StoppedProcessesView,
+    StopProcessView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
