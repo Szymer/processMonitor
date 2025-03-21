fe2c5652650fa17495ab6a89c47237d4af39f860
Szymon Zak <szymer@gmail.com>
2025-03-21 23:02:48 +0100

    b

--- process_monitor_app/admin.py
+++ process_monitor_app/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from .models import Process
 
+
 class ProcessAdmin(admin.ModelAdmin):
-    list_display = ('name', 'PID', 'status')
-    search_fields = ('name',)
+    list_display = ("name", "PID", "status")
+    search_fields = ("name",)
+
 
 admin.site.register(Process, ProcessAdmin)
