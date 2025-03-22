b7877ffcb5f752d52e93885d0b9cb9d621ea9f69
Szymon Zak <szymer@gmail.com>
2025-03-22 22:56:30 +0100

    black

--- process_monitor_app/tests/test_views.py
+++ process_monitor_app/tests/test_views.py
@@ -16,9 +16,7 @@ def test_process_list_view_access(client):
 @pytest.mark.django_db
 def test_process_list_view_authenticated_access(client, django_user_model):
     """Sprawdza, czy zalogowany użytkownik ma dostęp do listy procesów."""
-    django_user_model.objects.create_user(
-        username="testuser", password="password"
-    )
+    django_user_model.objects.create_user(username="testuser", password="password")
     client.login(username="testuser", password="password")  # Logujemy użytkownika
     url = reverse("process_list")
     response = client.get(url)
