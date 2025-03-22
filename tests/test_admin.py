a735de6b60f9cf565bb9f1725b9d5248223356e7
Szymon Zak <szymer@gmail.com>
2025-03-22 22:48:22 +0100

    black

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -2,7 +2,10 @@ import pytest
 from django.contrib.admin.sites import site
 from process_monitor_app.models import Process
 
+
 @pytest.mark.parametrize("model", [Process])
 def test_models_registered(model):
     """Sprawdza, czy modele są zarejestrowane w Django Admin."""
-    assert model in site._registry, f"{model.__name__} nie jest zarejestrowany w Django Admin"
+    assert (
+        model in site._registry
+    ), f"{model.__name__} nie jest zarejestrowany w Django Admin"
